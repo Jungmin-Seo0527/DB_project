4ad6bade5e95998263119cfc2918598647c8127b
Jungmin-Seo0527 <soato1405@naver.com>
2020-11-23 18:50:56 +0900

    naver_movie_lise
    title, price, rate, link, genre complete
    장르는 link정보를 통해 다시 접속하여야 장르를 확인할 수 있다.
    성인 인증이 필요한 영화들은 인증을 하고 장르를 확인할수 있기 때문에 우선 일괄적으로 19라고 표기함

--- crawlingpkg/naverMovie.py
+++ crawlingpkg/naverMovie.py
@@ -1,10 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 import sqlite3
 from selenium import webdriver
 import time
 
-# 우선 장르 보류
 # tite, price, rate, url
 def getNaverMovieList():
     for page in range(1, 6):
@@ -15,24 +15,53 @@ def getNaverMovieList():
         soup = BeautifulSoup(res.text, "lxml")
         movies=soup.find_all("li")
         for movie in movies:
+
+            # title
             title=movie.find("a", attrs={"class":"NPI=a:dcontent"})
             if title:
                 title=title["title"]
             else:
                 continue
-
-            price=movie.find("p", attrs={"class":"price2_book"})
+            
+            # price
+            # 어짜피 p태그는 동일 자매들중에 한개만 있음 따로 클래스 명으로 찾을 필요 없음
+            price=movie.find("p")
             if price:
-                price=price.find("span")
-                if price:
-                    price=price.get_text()
-                else:
-                    continue
+                price=price.find("span").get_text()
             else:
                 continue
             
+            # rate
+            rate=movie.find("em", attrs={"class":"score_num"})
+            if rate:
+                rate=rate.get_text()
+            else:
+                continue
+            
+            # link
+            link=movie.find("a", attrs={"class":"NPI=a:dcontent"})["href"]
+            link_head="https://serieson.naver.com/"
 
-            print(title+"->"+price)
+
+            # genre
+            # 19세는 url을 타고 들어가면 19게 인증을 해야 하기 때문에
+            # 일괄적으로 19라고 표기
+            genre_url=link_head+link
+            genre_res=requests.get(genre_url)
+            genre_res.raise_for_status()
+            genre_soup=BeautifulSoup(genre_res.text, "lxml")
+
+            genre=genre_soup.find("li", attrs={"class":"info_lst"})
+            if genre:
+                genre=genre.find("a").get_text()
+            else:
+                genre="19"
+                    
+            
+
+            print(f"title: {title} / price: {price} / rate: {rate} / link : {link_head+link} / genre : {genre}")
+            
+
 
 if __name__ == "__main__":
     getNaverMovieList()
